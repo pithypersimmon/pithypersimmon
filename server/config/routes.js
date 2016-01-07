59f293ee72581681c2d31f04fc13d198c8c81b66
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-01-06 20:15:15 -0800

    Backend changes to routes, server, and auth setting up google oauth

---
+++ server/config/routes.js
@@ -0,0 +1,31 @@
+var passport = require('passport');
+
+var helpers = require('./helpers.js');
+
+module.exports = function(app, express){
+	app.get('/', function(req, res){
+		res.send(200, 'main');
+	});
+
+	app.get('/login', function(req, res){
+		res.send(200, 'login');
+	});
+
+	app.get('/auth/google',
+  passport.authenticate('google', { 
+  	scope: 'https://www.googleapis.com/auth/plus.login' 
+  }));
+
+  app.get('/auth/google/return', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+  	console.log(req);
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+  app.get('/logout',function(req, res){
+  	req.logout();
+  	res.redirect('/login');
+  })
+}
