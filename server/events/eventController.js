9f8165d3b8ed5235287ac518191bd1dbfa8332a0
dangersong <dangersong1@gmail.com>
2016-01-09 14:20:54 -0800

    added event seed data and splash page event list with links

--- server/events/eventController.js
+++ server/events/eventController.js
@@ -5,35 +5,77 @@ var Event = require('./eventModel.js');
 var createEvent = Q.nbind(Event.create, Event);
 var findAllEvents = Q.nbind(Event.find, Event);
 
-module.exports = {
-	allEvents: function(req, res, next){
-		findAllEvents({})
-		.then(function(events){
-			res.json(events);
-		}).fail(function(error){
-			next(error);
-		})
-	},
+var seedData = [
+  {
+    title: 'Event1',
+    description: 'Event2 desc',
+    time: '1970-01-01T20:12:00.000Z',
+    date: '2020-01-01T08:00:00.000Z',
+    guestsCap: '2',
+    address: '123 Main ST',
+    city: 'San Francisco',
+    state: 'CA',
+    zip: '94107'
+  },
+  {
+    title: 'Event2',
+    description: 'Event2 desc',
+    time: '1970-01-01T20:12:00.000Z',
+    date: '2020-01-01T08:00:00.000Z',
+    guestsCap: '5',
+    address: '321 Main ST',
+    city: 'San Francisco',
+    state: 'CA',
+    zip: '94107'
+  },
+  {
+		title: 'Event3',
+		description: 'Event3 desc',
+		time: '1970-01-01T20:12:00.000Z',
+		date: '2020-01-01T08:00:00.000Z',
+		guestsCap: '10',
+		address: 'ABC Main ST',
+		city: 'San Francisco',
+		state: 'CA',
+		zip: '94107'
+  }
+];
 
-	addEvent: function(req, res, next){
-		console.log(req.body);
-		var title= req.body.title;
-		var description = req.body.description;
-		var time = req.body.time;
-		var date = req.body.date;
-		var guestsCap = req.body.guests;
-		var address = req.body.address;
-		var city = req.body.city;
-		var state = req.body.state;
-		var zip = req.body.zip;
-		
+module.exports = {
+  allEvents: function(req, res, next){
+    findAllEvents({})
+    .then(function(events){
+      if(events.length === 2) {
+        for(var i = 0; i < seedData.length; i ++) {
+        	console.log('hey now: ', seedData[i]);
+        	createEvent(seedData[i]);
+        };
+      }
+      console.log(events);
+      res.json(events);
+    }).fail(function(error){
+      next(error);
+    })
+  },
+
+  addEvent: function(req, res, next){
+    console.log(req.body);
+    var title= req.body.title;
+    var description = req.body.description;
+    var time = req.body.time;
+    var date = req.body.date;
+    var guestsCap = req.body.guests;
+    var address = req.body.address;
+    var city = req.body.city;
+    var state = req.body.state;
+    var zip = req.body.zip;
 
 		createEvent({
 			title: title,
 			description: description,
 			time: time,
 			date: date,
-			guestsCap: guestsCap, 
+			guestsCap: guestsCap,
 			address: address,
 			city: city,
 			state: state,
